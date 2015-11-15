abb57d3cf71bc4d89992abc79450040aafd5d3c8
Nirawit Jittipairoj <sixfingeredamish@gmail.com>
2015-11-14 23:42:39 -0800

    added mike's auth stuff, removed angular spotify, removed all old code, new design

--- README.md
+++ README.md
@@ -1,20 +1,26 @@
 # OneList.fm
 
 ##What is the purpose of your project (i.e., why would someone use this app)?
-OneList was created with the philosophy that anyone should be able to enjoy all their music everywhere. Instead of having some of your favorite music saved in a playlist in Spotify, some in an Rdio playlist, and so on.
 
-OneList uses a text file containing a list of the user's favorite songs and artists and presents the user with a list of streaming platforms the user can choose to use. This way, a user doesn't have to settle with created a favorite list in Rdio and manually create another one in Spotify, of course also updating each list manually. OneList fixes that—having a single list of your favorite music everywhere solves this problem.
+OneList was created with the philosophy that anyone should be able to enjoy all their music everywhere, on every platform and service. The only way to achieve this is true the web that allows the user to play their music on any streaming service like Rdio and Spotify. Instead of having some of your favorite music saved in a Spotify playlist, some in an Rdio playlist, and others on YouTube, users have a single list which they can continue adding to as they venture on their music journey.
 
-While this version only supports a text file, the future applications, once implemented, are endless: the user could select a file from Dropbox, Simplenote, or Evernote, so that their favorite music are truly everywhere.
+OneList uses a text file containing a list of the user's favorite songs and artists and presents the user with a list of streaming platforms the user can choose to use. This way, a user doesn't have to settle with created a favorite list in Rdio and manually create another one in Spotify, of course also updating each list manually. OneList fixes that. Having a single list of your favorite music everywhere solves this problem.
+
+This version only supports text input. In the future, having one list everywhere will be painless. The user could select a file from Dropbox, Simplenote, or Evernote, so that their favorite music are truly everywhere and they no longer need to visit the site to update their music.
 
 ##What is the URL of this project on your student web-server?
 
+apple.com/music nope
 
 ##Did you receive help from any other sources (classmates, etc.)? If so, please list who.
-Walter Ceder (webapp genius), StackOverflow (every error possible), JoshMorony.com (his Spotify login code)
+Walter Ceder (webapp genius), StackOverflow (has every error possible), and Susan (somehow loves JS more than Java). Mike Freeman's Firebase Twitter demo code.
 
 ##Approximately how many hours did it take you to complete this challenge?
-20
+
+20 hours. However, 10 were spent writing code that I had to trash, like trying to get angular-spotify to work and wrestling with authentication and OAuth.
 
 ##Did you encounter any problems in this challenge we should warn students about in the future? How can we make the challenge better?
-Not specific to all students, but they might need to learn how OAuth works to get Spotify login to work and store token. This took a really long time for me.
+
+Not specific to all students, but they might need to learn how OAuth works to get Spotify login to work and store token. This took a really long time for me and in the end little things like handling token still didn't work so I had to ditch it. However, others more fluent in code and web APIs may not encounter this problem.
+
+PrivacyBadger (an extension made by the EFF) blocked AngularFire and Firebase CDNs but only when running from localhost. Super weird. Took a while to troubleshoot this.
--- main.js
+++ main.js
@@ -1,36 +1,142 @@
-var app = angular.module('app', ['spotify']);
+var data;
+//utlize spotify api search
+var baseUrl = 'https://api.spotify.com/v1/search?type=track&query=';
 
-app.config(function (SpotifyProvider) {
-  SpotifyProvider.setClientId("7800dc3be577424e9ec87e9917bd41d3");
-  SpotifyProvider.setRedirectUri("http://localhost:8080/callback.html");
-  SpotifyProvider.setScope("user-read-private playlist-read-private playlist-modify-private playlist-modify-public");
-  SpotifyProvider.setAuthToken("token");
-})
+var myApp = angular.module('myApp', ['firebase']);
+myApp.controller('MainController', function($scope, $firebaseAuth, $firebaseArray, $firebaseObject, $http){
 
-app.controller('MainController', ['$scope', 'Spotify', function ($scope, Spotify) {
-  $scope.login = function () {
-    Spotify.login();
-  };
+  var ref = new Firebase("https://onelistfm.firebaseio.com");
 
-  $scope.userId = function () {
-    Spotify.getCurrentUser();
+  var accountsRef = ref.child("accounts");
+  var listsRef = ref.child("lists");
+
+  $scope.accounts = $firebaseObject(accountsRef);
+  $scope.lists = $firebaseArray(listsRef);
+
+  $scope.listClicked = false;
+
+  $scope.clicked = function(songList) {
+    $scope.songs = [];
+    $scope.track = "";
+    $scope.tracks = "";
+
+    if (songList.songs != 0) {
+      var listUrl = new Firebase("https://onelistfm.firebaseio.com/songList/" + songList.$id + "/" + songList.songs);
+      var playlist = $firebaseArray(baz);
+      $scope.songs = playlist;
+    }
+  }
+  $scope.authObj = $firebaseAuth(ref);
+
+  //mike's login stuff
+  // Test if already logged in
+  var authData = $scope.authObj.$getAuth();
+  if (authData) {
+    $scope.userId = authData.uid;
   }
 
-  $scope.searchArtist = function () {
-    Spotify.search($scope.searchartist, 'artist').then(function (data) {
-      $scope.artists = data.artists.items;
+  // SignUp function
+  $scope.signUp = function() {
+    // Create user
+    $scope.authObj.$createUser({
+      email: $scope.email,
+      password: $scope.password,
+    })
+
+    // Once the user is created, call the logIn function
+    .then($scope.logIn)
+
+    // Once logged in, set and save the user data
+    .then(function(authData) {
+      $scope.userId = authData.uid;
+      $scope.accounts[authData.uid] ={
+        username:$scope.username
+      }
+      $scope.accounts.$save()
+    })
+
+  // Catch any errors
+    .catch(function(error) {
+      console.error("Error: ", error);
     });
-  };
-
-  $scope.createPlaylist = function () {
-    Spotify.createPlaylist(userId, { name: 'OneList' });
   }
 
-  // Spotify.createPlaylist(':userid', { name: 'OneList' }).then(function (data) {
-  // console.log('playlist created');
+  // SignIn function
+  $scope.signIn = function() {
+    $scope.logIn().then(function(authData){
+      $scope.userId = authData.uid;
+    })
+  }
 
+  // LogIn function
+  $scope.logIn = function() {
+    return $scope.authObj.$authWithPassword({
+      email: $scope.email,
+      password: $scope.password
+    })
+  }
 
-// });
+  // LogOut function
+  $scope.logOut = function() {
+    $scope.authObj.$unauth()
+    $scope.userId = false
+    $scope.username = "";
+    $scope.email = "";
+    $scope.password = "";
+  }
 
+  //create new playlist
+  $scope.create = function() {
+    $scope.lists.$add({
+      title: $scope.listName,
+      userId: $scope.userId,
+      songs: 0,
+      time: Firebase.ServerValue.TIMESTAMP
+    }).then(function() {
+      $scope.listName = "";
+      $scope.lists.$save();
+    })
+  }
 
-}]);
+  //spotify search
+  $scope.audioObject = {};
+  $scope.search = function() {
+    $http.get(baseUrl + $scope.track).success(function(response){
+      data = $scope.tracks = response.tracks.items;
+      console.log($scope.track);
+      console.log(data);
+    })
+  }
+
+  //play song preview
+  $scope.play = function(song) {
+    if($scope.currentSong == song) {
+      $scope.audioObject.pause();
+      $scope.currentSong = false;
+      return
+    }
+    else {
+      if($scope.audioObject.pause != undefined) $scope.audioObject.pause()
+      $scope.audioObject = new Audio(song);
+      $scope.audioObject.play();
+      $scope.currentSong = song;
+    }
+  }
+
+  //add song to queue
+  $scope.addSong = function(songList, track) {
+    if (songList.songs == 0) { //if list empty
+      var listUrl = new Firebase("https://onelistfm.firebaseio.com/lists/" + songList.$id);
+      var tempList = listUrl.push([]);
+      songList.songs = tempList.key();
+      $scope.lists.$save(songList);
+    }
+    $scope.songs = [];
+    var listUrl = new Firebase("https://onelistfm.firebaseio.com/lists/" + songList.$id + "/" + songList.songs);
+    var tempList = listUrl.push(angular.copy(track));
+    var playlist = $firebaseArray(tempList);
+    $scope.songs = playlist;
+
+    console.log(playlist);
+  }
+})
